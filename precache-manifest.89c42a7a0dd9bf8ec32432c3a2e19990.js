1484d53be5f020d93ff4e359979a24f9b6fd03f8
thevaibhavmishra <vaibhavmishra5013@gmail.com>
2022-10-10 12:25:33 +0530

    Updates

---
+++ precache-manifest.89c42a7a0dd9bf8ec32432c3a2e19990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc924ad588e5d6e5a54",
+    "url": "/static/css/main.cf6253ef.chunk.css"
+  },
+  {
+    "revision": "bfc924ad588e5d6e5a54",
+    "url": "/static/js/main.bfc924ad.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "22d62e2ec69a33cd234b1188be36853e",
+    "url": "/index.html"
+  }
+];
